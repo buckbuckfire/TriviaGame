784d7e201362f2d567cf2170ae29188b501126f5
Matt Valle <mattvalle28@gmail.com>
2019-10-19 15:59:31 -0400

    html, java

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 var timeLeft = 30;
 var elem = document.getElementById('time-left');
 var timerId = setInterval(countdown, 1000);
+var winCount = 0;
+var lossCount = 0;
 
 function countdown() {
     if (timeLeft == -1) {
@@ -15,4 +17,58 @@ function countdown() {
 function doSomething() {
     alert("Hi");
 }
+
+
+// question 1
+$("#button1").click(function(){
+    alert("test");
+});
+
+$("#button2").click(function(){
+    alert("test");
+});
+
+$("#button3").click(function(){
+    alert("test");
+});
+
+$("#button4").click(function(){
+    alert("test");
+});
+
+//question 2
+
+$("#button1_2").click(function(){
+    alert("test");
+});
+
+$("#button2_2").click(function(){
+    alert("test");
+});
+
+$("#button3_2").click(function(){
+    alert("test");
+});
+
+$("#button4_2").click(function(){
+    alert("test");
+});
+
+//question 3
+
+$("#button1_3").click(function(){
+    alert("test");
+});
+
+$("#button2_3").click(function(){
+    alert("test");
+});
+
+$("#button3_3").click(function(){
+    alert("test");
+});
+
+$("#button3_4").click(function(){
+    alert("test");
+});
 
